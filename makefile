632142d6210f5b4121d3b4b135137e41a5d92ea3
D_Pain <coreymu@udel.edu>
2018-09-05 18:37:52 +0000

    Improved example code. markovian.c now has some starting code. Reads the KJV bible for the markovian chain that will be used.

--- makefile
+++ makefile
@@ -1,7 +1,8 @@
 CC=gcc
+CFLAGS="-std=c99"
 	
 markovian: markovian.c;
-	$(CC) -o a.out markovian.c `pkg-config --cflags --libs glib-2.0`
+	$(CC) $(CFLAGS) -o a.out markovian.c `pkg-config --cflags --libs glib-2.0`
 
 clean:
 	rm *.o
--- markovian.c
+++ markovian.c
@@ -1,8 +1,72 @@
 #include <glib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
+#define BUFFER_SIZE 1024
+#define SKIP_FIRST_LINE 13
+#define MAX_READ_LINE 2
+
+int isDigitsPunct(const char *str) {
+    while (*str) {
+        if (isdigit(*str) == 0 && ispunct(*str) == 0) {
+            return 0;
+        }
+        str++;
+    }
+    return 1;
+}
 
 int main(void) {
-    GHashTable *hash = g_hash_table_new(g_str_hash, g_str_equal);
+    GHashTable *words = g_hash_table_new(g_str_hash, g_str_equal);
+    
+    // Buffer used to read the file
+    char str[BUFFER_SIZE];
+    
+    //Reading duh Bible
+    FILE * fp = fopen("project_gutenberg_the_king_james_bible.txt", "r");
+    if (fp == NULL) {
+        printf("File does not exist!\n");
+        exit(1);
+    } else {
+        printf("File exists!\n");
+    }
+
+    // Skip the first few lines since it does not contain content
+    for(int k = 0; k < SKIP_FIRST_LINE; k++) {
+        fgets(str, BUFFER_SIZE, fp);
+    }
+    
+    int line_num = 0;
+    char *prevWord;
+    while (fgets(str, BUFFER_SIZE, fp) && line_num < MAX_READ_LINE) {
+        printf("line: %s\n", str);
+        int size = strlen(str);
+
+        printf("Splitting string \"%s\" into words:\n", str);
+        char *word = strtok(str, " ");
+        while (word != NULL) {
+            if(!isDigitsPunct(word)) {
+                printf("word: %s\n", word);
+            }
+            
+            if(prevWord != NULL) {
+                // Probability distribution job here
+                if(g_hash_table_contains(words, prevWord)) {
+                    //int count = g_hash_table_lookup(g_hash_table_lookup(words, prevWord), word);
+                } else {
+                    //(words, prevWord, (gpointer) g_hash_table_lookup(words, prevWord), word);
+                }
+            }
+            
+            prevWord = word;
+            word = strtok(NULL, " ");
+        }
+        line_num++;
+    }
+    
+    /**
+    
     
     GHashTable *prob_dist1 = g_hash_table_new(g_str_hash, g_str_equal);
     g_hash_table_insert(prob_dist1, "Virginia", GINT_TO_POINTER(20));
@@ -15,20 +79,13 @@ int main(void) {
     g_hash_table_insert(prob_dist2, "Texas", GINT_TO_POINTER(50));
     g_hash_table_insert(prob_dist2, "Ohio", GINT_TO_POINTER(40));
     
-    g_hash_table_insert(hash, "1", (gpointer) prob_dist1);
-    g_hash_table_insert(hash, "2", (gpointer) prob_dist2);
-    printf("There are %d keys in the hash\n", g_hash_table_size(hash));
-    printf("There are %d keys in the prob_dist1\n", g_hash_table_size(prob_dist1));
-    
-    printf("There are %d keys in the prob_dist1 which is in 1\n", g_hash_table_size(g_hash_table_lookup(hash, "1")));
-    printf("Value is %d for the key: %s in the prob_dist1\n", g_hash_table_lookup(g_hash_table_lookup(hash, "1"), "Delaware"), "Delaware");
-    
-    printf("There are %d keys in the prob_dist2 which is in 2\n", g_hash_table_size(g_hash_table_lookup(hash, "2")));
-    printf("Value is %d for the key: %s in the prob_dist2\n", g_hash_table_lookup(g_hash_table_lookup(hash, "2"), "Virginia"), "Virginia");
+    g_hash_table_insert(words, "1", (gpointer) prob_dist1);
+    g_hash_table_insert(words, "2", (gpointer) prob_dist2);
     
     g_hash_table_destroy(prob_dist1);
     g_hash_table_destroy(prob_dist2);
-    g_hash_table_destroy(hash);
+    g_hash_table_destroy(words);
+    */
     
     return 0;
 }
