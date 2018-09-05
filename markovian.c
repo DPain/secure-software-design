a1d96cb891c8faf1800d081ee89d605cdf004036
D_Pain <coreymu@udel.edu>
2018-09-05 18:37:52 +0000

    Improved example code.

--- markovian.c
+++ markovian.c
@@ -1,5 +1,5 @@
 #include <glib.h>
-
+#include <stdio.h>
 
 int main(void) {
     GHashTable *hash = g_hash_table_new(g_str_hash, g_str_equal);
@@ -17,14 +17,14 @@ int main(void) {
     
     g_hash_table_insert(hash, "1", (gpointer) prob_dist1);
     g_hash_table_insert(hash, "2", (gpointer) prob_dist2);
-    printf("There are %d keys in the hash\n", g_hash_table_size(hash));
-    printf("There are %d keys in the prob_dist1\n", g_hash_table_size(prob_dist1));
+    printf("There are %u keys in the hash\n", g_hash_table_size(hash));
+    printf("There are %u keys in the prob_dist1\n", (unsigned int) g_hash_table_size(prob_dist1));
     
     printf("There are %d keys in the prob_dist1 which is in 1\n", g_hash_table_size(g_hash_table_lookup(hash, "1")));
-    printf("Value is %d for the key: %s in the prob_dist1\n", g_hash_table_lookup(g_hash_table_lookup(hash, "1"), "Delaware"), "Delaware");
+    printf("Value is %d for the key: %s in the prob_dist1\n", GPOINTER_TO_INT(g_hash_table_lookup(g_hash_table_lookup(hash, "1"), "Delaware")), "Delaware");
     
     printf("There are %d keys in the prob_dist2 which is in 2\n", g_hash_table_size(g_hash_table_lookup(hash, "2")));
-    printf("Value is %d for the key: %s in the prob_dist2\n", g_hash_table_lookup(g_hash_table_lookup(hash, "2"), "Virginia"), "Virginia");
+    printf("Value is %d for the key: %s in the prob_dist2\n", GPOINTER_TO_INT(g_hash_table_lookup(g_hash_table_lookup(hash, "2"), "Virginia")), "Virginia");
     
     g_hash_table_destroy(prob_dist1);
     g_hash_table_destroy(prob_dist2);
